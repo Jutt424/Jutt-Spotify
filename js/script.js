1e82f65607d9d6fb9155328356cfca1a8369d58d
Muhammad Azhar <144532814+Jutt424@users.noreply.github.com>
2024-01-13 15:59:57 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ function secondsToMinutesSeconds(seconds) {
 async function getSongs(folder) {
     try {
         currFolder = folder;
-        let response = await fetch(`http://192.168.0.108:3000//${folder}/`);
+        let response = await fetch(`https://musifyhub.netlify.app//${folder}/`);
         let html = await response.text();
         let parser = new DOMParser();
         let doc = parser.parseFromString(html, "text/html");
