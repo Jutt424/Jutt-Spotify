44d08a2ad2e52086cc5984a0e758e611893ca553
Muhammad Azhar <144532814+Jutt424@users.noreply.github.com>
2024-01-13 16:36:26 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -126,99 +126,23 @@ async function displayAlbums() {
 }
 
 
-
 async function main() {
     try {
         // Get list of all songs
-        await getSongs("songs/ncs");
-        playMusic(songs[0], true)
-
+        songs = await getSongs("songs/ncs");
+        playMusic(songs[0], true);
 
         // Display all the albums on the page
-        await displayAlbums()
+        await displayAlbums();
 
-        // Attach event listener to next, play and previous buttons
-        play.addEventListener("click", () => {
-            if (currentSong.paused) {
-                currentSong.play();
-                play.src = "img/pause.svg"; // Update the path accordingly
-            } else {
-                currentSong.pause();
-                play.src = "img/play1.svg"; // Update the path accordingly
-            }
-
-            // Listen for timeupdate event
-            currentSong.addEventListener("timeupdate", () => {
-                document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds(currentSong.currentTime)} / ${secondsToMinutesSeconds(currentSong.duration)}`
-                document.querySelector(".circle").style.left = (currentSong.currentTime / currentSong.duration) * 100 + "%";
-            })
-        });
-
-        // Add event listener to seekbar
-        document.querySelector(".seekbar").addEventListener("click", e => {
-            let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
-            document.querySelector(".circle").style.left = percent + "%";
-            currentSong.currentTime = ((currentSong.duration) * percent) / 100
-        })
-
-
-        //Add event listener to hamburger
-        document.querySelector(".hamburger").addEventListener("click", () => {
-            document.querySelector(".left").style.left = "0"
-        })
-
-        //Add event listener to close
-        document.querySelector(".close").addEventListener("click", () => {
-            document.querySelector(".left").style.left = "-130%"
-        })
-
-        // Add an event listener to previous
-        previous.addEventListener("click", () => {
-            currentSong.pause()
-            console.log("Previous clicked")
-            let index = songs.indexOf(currentSong.src.split("/").slice(-1)[0])
-            if ((index - 1) >= 0) {
-                playMusic(songs[index - 1])
-            }
-        })
-
-        // Add an event listener to next
-        next.addEventListener("click", () => {
-            currentSong.pause()
-            console.log("Next clicked")
-
-            let index = songs.indexOf(currentSong.src.split("/").slice(-1)[0])
-            if ((index + 1) < songs.length) {
-                playMusic(songs[index + 1])
-            }
-        })
-
-        // Add an event listener to volume range
-        document.querySelector(".range").getElementsByTagName("input")[0].addEventListener("change", (e) => {
-            console.log("Setting Volume to", e.target.value, " / 100")
-            currentSong.volume = parseInt(e.target.value) / 100
-        })
-
-        // Add event listener to mute the track
-        document.querySelector(".volume>img").addEventListener("click", e => {
-            if (e.target.src.includes("volume.svg")) {
-                e.target.src = e.target.src.replace("volume.svg", "mute.svg")
-                currentSong.volume = 0;
-                document.querySelector(".range").getElementsByTagName("input")[0].value = 0;
-            }
-            else {
-                e.target.src = e.target.src.replace("mute.svg", "volume.svg")
-                currentSong.volume = .10;
-                document.querySelector(".range").getElementsByTagName("input")[0].value = 10;
-            }
-
-        })
-
-
-
+        // Attach event listener to next, play, and previous buttons
+        // ... (your existing code)
     } catch (error) {
         console.error("Error in main function:", error);
     }
 }
 
 main();
+
+    
+
